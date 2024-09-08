d54109f3b18fcbf1413505fd4e0d55a2d79eb28e
zakariacheknane <cheknanezakaria@gmail.com>
2024-09-08 22:45:05 +0100

    Create first version of header

--- ClientPart/src/App.jsx
+++ ClientPart/src/App.jsx
@@ -5,6 +5,7 @@ import Signin from './pages/Signin';
 import About from './pages/About';
 import SignUp from './pages/SignUp';
 import { Profiler } from 'react';
+import Header from './components/Header';
 
 export default function App() {
   return (
