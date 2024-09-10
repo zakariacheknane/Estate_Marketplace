34c5fe818fb954f8146c66e093b54c9a7afa00b4
zakariacheknane <cheknanezakaria@gmail.com>
2024-09-10 07:12:46 +0100

    Create a test controller

--- ServerPart/routes/user.route.js
+++ ServerPart/routes/user.route.js
@@ -1,11 +1,8 @@
 import express from 'express';
+import { test } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
-router.get('/test', (req, res) => {
-    res.json({
-      message:'test api'});
-  
-  })
+router.get('/test', test);
 
   export default router;
