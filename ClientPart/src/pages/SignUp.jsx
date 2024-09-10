13666b3f0f5a5dc7c8a4f2b9d33220c7995d50ec
zakariacheknane <cheknanezakaria@gmail.com>
2024-09-10 23:12:18 +0100

    Add handleChange

--- ClientPart/src/pages/SignUp.jsx
+++ ClientPart/src/pages/SignUp.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
+
 export default function SignUp() {
+
+  const [formData, setFormData] = useState({});
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className="text-3xl text-center font-semibold my-7">SignUp</h1>
@@ -10,21 +19,21 @@ export default function SignUp() {
           placeholder='username'
           className='border p-3 rounded-lg'
           id='username'
-         
+          onChange={handleChange}
         />
          <input
           type='email'
           placeholder='email'
           className='border p-3 rounded-lg'
           id='email'
-       
+          onChange={handleChange}
         />
         <input
           type='password'
           placeholder='password'
           className='border p-3 rounded-lg'
           id='password'
-       
+          onChange={handleChange}
         />
           <button
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
