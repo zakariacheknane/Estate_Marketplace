204cfb0b427bb96c558bd3ee7dd269dcc0033166
zakariacheknane <cheknanezakaria@gmail.com>
2024-09-12 03:31:35 +0100

    Add Google OAuth functionality to Client Part

--- ClientPart/src/components/OAuth.jsx
+++ ClientPart/src/components/OAuth.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { app } from '../firebase';
+import { signInSuccess } from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 export const OAuth = () => {
-
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
   const handleGoogleClick = async () => {
     try{
-
+        const provider = new GoogleAuthProvider();
+        const auth = getAuth(app);
+        const result = await signInWithPopup(auth, provider);
+        const res = await fetch('/api/auth/google', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              name: result.user.displayName,
+              email: result.user.email,
+              photo: result.user.photoURL,
+            }),
+          });
+          const data = await res.json();
+          dispatch(signInSuccess(data));
+          navigate('/');
     }
     catch (error) {
         console.log('could not sign in with google', error);
