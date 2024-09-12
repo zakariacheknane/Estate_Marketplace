754e8b90e1a80e2b85d4075dab698aa61cd2d9b3
zakariacheknane <cheknanezakaria@gmail.com>
2024-09-12 03:10:19 +0100

    create Oauth component

--- ClientPart/src/pages/Signin.jsx
+++ ClientPart/src/pages/Signin.jsx
@@ -6,6 +6,7 @@ import {
   signInSuccess,
   signInFailure,
 } from '../redux/user/userSlice';
+import { OAuth } from "../components/OAuth";
 
 export default function Signin() {
   const [formData, setFormData] = useState({});
@@ -64,8 +65,9 @@ export default function Signin() {
            disabled={loading}
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
-        {loading ? 'Loading...' : 'SIGN IN'}
+        {loading ? 'Loading...' : 'Sign in'}
         </button>
+        <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Dont Have an account?</p>
