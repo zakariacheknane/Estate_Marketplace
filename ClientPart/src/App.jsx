687e8bf0a4360e598eed1ad8f111ec7bbba4254c
zakariacheknane <cheknanezakaria@gmail.com>
2024-09-14 21:27:24 +0100

    Complete create listing page Ul

--- ClientPart/src/App.jsx
+++ ClientPart/src/App.jsx
@@ -8,6 +8,7 @@ import SignUp from './pages/SignUp';
 import Header from './components/Header';
 import Profile from './pages/Profile';
 import PrivateRoute from './components/PrivateRoute';
+import CreateListing from './pages/CreateListing';
 
 export default function App() {
   return (
@@ -20,6 +21,7 @@ export default function App() {
       <Route path='/about' element={<About />} />
       <Route element={<PrivateRoute />}>
       <Route path='/profile' element={<Profile />} />
+      <Route path='/create-listing' element={<CreateListing/>} />
     </Route>
     </Routes>
   </BrowserRouter>
