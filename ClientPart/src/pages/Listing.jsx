46a119e7889ccc26945a8a94bbc2f10a85b4606c
zakariacheknane <cheknanezakaria@gmail.com>
2024-09-20 22:22:02 +0100

    Create Listing.jsx

---
+++ ClientPart/src/pages/Listing.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+
+export const Listing = () => {
+    const [listing, setListing] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const params = useParams();
+    useEffect(() => {
+        const fetchListing = async () => {
+          try {
+            setLoading(true);
+            const res = await fetch(`/api/listing/get/${params.listingId}`);
+            const data = await res.json();
+            if (data.success === false) {
+              setError(true);
+              setLoading(false);
+              return;
+            }
+            setListing(data);
+            setLoading(false);
+            setError(false);
+          } catch (error) {
+            setError(true);
+            setLoading(false);
+          }
+        };
+        fetchListing();
+      }, [params.listingId]);
+  return (
+    <main>
+        {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
+        {error && (
+        <p className='text-center my-7 text-2xl'>Something went wrong!</p>
+      )}
+        {listing && !loading && !error && (
+            
+        )}
+    </main>
+  )
+}
