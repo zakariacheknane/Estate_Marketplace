21e579b01d768ec2327a71686df6fe9f556d0216
zakariacheknane <cheknanezakaria@gmail.com>
2024-09-20 15:12:10 +0100

    Complete delete user listing functionality

--- ClientPart/src/pages/Profile.jsx
+++ ClientPart/src/pages/Profile.jsx
@@ -135,7 +135,24 @@ export default function Profile() {
       setShowListingsError(true);
     }
   };
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
 
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Profile</h1>
@@ -247,13 +264,12 @@ export default function Profile() {
 
               <div className="flex flex-col item-center">
                 <button
+                  onClick={() => handleListingDelete(listing._id)}
                   className="text-red-700 uppercase"
                 >
                   Delete
                 </button>
-
                   <button className="text-green-700 uppercase">Edit</button>
-               
               </div>
             </div>
           ))}
