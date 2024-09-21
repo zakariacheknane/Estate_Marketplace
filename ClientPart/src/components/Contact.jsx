46adc305c9e5b42449770882b10b54f22d7c9f1c
zakariacheknane <cheknanezakaria@gmail.com>
2024-09-21 20:24:29 +0100

    Create fetchLandlord

--- ClientPart/src/components/Contact.jsx
+++ ClientPart/src/components/Contact.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export const Contact = () => {
+export const Contact = ({ listing }) => {
+    const [landlord, setLandlord] = useState(null);
+    useEffect(() => {
+        const fetchLandlord = async () => {
+          try {
+            const res = await fetch(`/api/user/${listing.userRef}`);
+            const data = await res.json();
+            setLandlord(data);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        fetchLandlord();
+      }, [listing.userRef]);
   return (
     <div>Contact</div>
   )
