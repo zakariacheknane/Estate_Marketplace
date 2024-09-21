1e5cece6dd746bf727a423e07477bcbc274581c7
zakariacheknane <cheknanezakaria@gmail.com>
2024-09-21 02:13:08 +0100

    Add image slider to the listing page

--- ClientPart/src/pages/Listing.jsx
+++ ClientPart/src/pages/Listing.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
-
+import { Swiper, SwiperSlide } from 'swiper/react'
+import SwiperCore from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css/bundle';
 export const Listing = () => {
+    SwiperCore.use([Navigation]);
     const [listing, setListing] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
@@ -34,7 +38,19 @@ export const Listing = () => {
         <p className='text-center my-7 text-2xl'>Something went wrong!</p>
       )}
         {listing && !loading && !error && (
-            
+                  <Swiper navigation>
+                  {listing.imageUrls.map((url) => (
+                    <SwiperSlide key={url}>
+                      <div
+                        className='h-[550px]'
+                        style={{
+                          background: `url(${url}) center no-repeat`,
+                          backgroundSize: 'cover',
+                        }}
+                      ></div>
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
         )}
     </main>
   )
