cbf0c7554eaaa8daad1a9d6c6117106b3869a474
zakariacheknane <cheknanezakaria@gmail.com>
2024-09-23 18:56:25 +0100

    Add show more listings functionality

--- ClientPart/src/pages/Search.jsx
+++ ClientPart/src/pages/Search.jsx
@@ -49,6 +49,7 @@ export const Search = () => {
     }
     const fetchListings = async () => {
       setLoading(true);
+      setShowMore(false)
       const searchQuery = urlParams.toString();
       const res = await fetch(`/api/listing/get?${searchQuery}`);
       const data = await res.json();
@@ -109,6 +110,19 @@ export const Search = () => {
     urlParams.set("order", sidebardata.order);
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
+  };
+  const onShowMoreClick = async () => {
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('startIndex', startIndex);
+    const searchQuery = urlParams.toString();
+    const res = await fetch(`/api/listing/get?${searchQuery}`);
+    const data = await res.json();
+    if (data.length < 9) {
+      setShowMore(false);
+    }
+    setListings([...listings, ...data]);
   };
   return (
     <div className="flex flex-col md:flex-row">
